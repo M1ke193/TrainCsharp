cdbd2d93cce00ea428d6206e1ea1e1c52f465619
M1ke193 <mndx1t@gmail.com>
2024-04-28 21:17:34 +0700

    ref and out key

---
+++ out-key/Program.cs
@@ -0,0 +1,20 @@
+﻿// Từ khóa out
+void func(out string outKeyVariable)
+{
+    outKeyVariable = "Init Out key"; // init giá trị tại đây
+    outKeyVariable = outKeyVariable + " And use it";
+}
+string outKeyVariable; //không cần init
+// Có thể không cần khởi tạo biến outKeyVariable mà dùng thẳng func(out string outKeyVariable) luôn
+func(out outKeyVariable);
+Console.WriteLine(outKeyVariable);
+
+// Từ khóa ref
+void func2(ref int refKeyVariable)
+{
+    refKeyVariable = refKeyVariable + 5;
+}
+int refKeyVariable = 5; //tạo biến và khởi tạo giá trị cho biến
+func2(ref refKeyVariable);
+Console.WriteLine(refKeyVariable);
+Console.ReadLine();
